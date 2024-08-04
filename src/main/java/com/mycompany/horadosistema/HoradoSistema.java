12808676bbfc144b1850bc9fe9d9fd44c27065db
mathe <mathe@DESKTOP-B060MMV>
2024-08-04 17:44:39 -0300

    Primeiro Commit!

---
+++ src/main/java/com/mycompany/horadosistema/HoradoSistema.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.horadosistema;
+
+/**
+ *
+ * @author mathe
+ */
+public class HoradoSistema {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
